9cb10aece2e80444a24fb69bff70dbc8e145c3a2
Parto Karwat <karwat-b@gmx.de>
2016-07-13 07:56:12 +0200

    add: scrollview and simplified code

--- MyAppPortfolio/app/src/main/java/com/fanrir/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/fanrir/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.fanrir.myappportfolio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,27 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void launchPopularMovies(View view) {
-        Toast.makeText(this, R.string.launch_popular_movies_toast, Toast.LENGTH_SHORT).show();
+    public void showToast(View view) {
+        Button button = (Button) view;
+        String appName = button.getText().toString();
+        Toast.makeText(this, getString(R.string.launch_app_toast, appName), Toast.LENGTH_SHORT).show();
     }
 
-    public void launchStockHawk(View view) {
-        Toast.makeText(this, R.string.launch_stock_hawk_toast, Toast.LENGTH_SHORT).show();
-    }
-
-    public void launchBuildItBigger(View view) {
-        Toast.makeText(this, R.string.launch_build_it_bigger_toast, Toast.LENGTH_SHORT).show();
-    }
-
-    public void launchMakeYourAppMaterial(View view) {
-        Toast.makeText(this, R.string.launch_make_your_app_material_toast, Toast.LENGTH_SHORT).show();
-    }
-
-    public void launchGoUbiquitous(View view) {
-        Toast.makeText(this, R.string.launch_go_ubiquitous_toast, Toast.LENGTH_SHORT).show();
-    }
-
-    public void launchCapstone(View view) {
-        Toast.makeText(this, R.string.launch_capstone_toast, Toast.LENGTH_SHORT).show();
-    }
 }
